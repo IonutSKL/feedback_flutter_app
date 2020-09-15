d21c9e9378e1805437999937b9d322639abd354f
IonutScarlat <scarlatalinn@gmail.com>
2020-09-15 23:21:43 +0200

    widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Real News Romania'),);
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First app'),
+        ),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
