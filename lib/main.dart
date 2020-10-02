91dcd9cac6acd921c781058ee5e24a868a4a9ec8
IonutScarlat <scarlatalinn@gmail.com>
2020-10-02 18:19:51 +0200

    question widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './question.dart';
 void main() {
   runApp(MyApp());
 }
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Red'),
               onPressed: _answerQuestion,
